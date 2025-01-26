1d1993214c8bde587d85b4e2820fabb047068bb7
imoncrack27 <mingodrexell321@gmail.com>
2025-01-26 22:08:03 +0800

    added admin and user dashboards with logout button on admin

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/home', [HomeController::class, 'index'])->name('home');
+
+
+
 
 Route::middleware([
     'auth:sanctum',
